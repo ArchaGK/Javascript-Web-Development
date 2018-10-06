fd2a4b6ea83662902e074ee7e71ae51d7f9b5f16
ArchaGK <archaghodge@gmail.com>
2018-10-06 18:28:10 +0200

    Updating Chess board game

--- chess-board-game/Readme.md
+++ chess-board-game/Readme.md
@@ -1,4 +1,10 @@
 This project is about Multiplayer chess board game built
 using chess.js, ChessBoard.js and socket.IO
 
+Main files for this project
+----------------------------
+Public/default.js
+app.js
+
+
 [![chess.png](https://i.postimg.cc/7LFKbYJf/chess.png)](https://postimg.cc/rd91PTJk)
--- chess-board-game/public/default.js
+++ chess-board-game/public/default.js
@@ -10,30 +10,35 @@ window.onload = function(){
 var socket = io();
 
 var initGame = function () {
+    // Configuration
     var cfg = {
         draggable: true,
         position: 'start',
         onDrop: handleMove,
     };
 
+    // Setup the Board
     board = new ChessBoard('gameBoard', cfg);
+    // Game engine rules
     game = new Chess();
 };
 
+// Track the movement of pieces and emit the
+// move made to the opponent.
 var handleMove = function(source, target) {
+
     var move = game.move({from: source, to: target});
 
-    if(move === null) return 'snapback';
-
-    else socket.emit("move", move);
-
+    if(move === null)
+        return 'snapback';
+    else
+        socket.emit("move", move);
 
 };
 
-
-
+// Capture the opponent's move and update
+// board position
 socket.on('move',function(msg){
-
   game.move(msg);
   board.position(game.fen());
 });
